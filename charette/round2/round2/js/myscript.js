0622861b3b2e975d0eee218defe1372e19dbd4db
itylermcmanus <tyler.mcmanus1@icloud.com>
2016-09-12 10:25:18 -0500

    Add files via upload

---
+++ charette/round2/round2/js/myscript.js
@@ -0,0 +1,434 @@
+$(document).ready(function() {
+
+
+	$('#box1').click(function() {
+		$(this).animate({backgroundColor:'#ffd700'}, 3000);
+		
+	});
+
+	$('#box2').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("pulse" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box3').click(function() {
+		$(this).animate({marginLeft:'5000px'}, 5000);
+		$(this).animate({height:'50px'}, 5000);
+		$(this).animate({width:'50px'}, 5000);
+		
+	});
+
+	$('#box4').click(function() {
+		$(this).animate({marginLeft:'5000px'}, 2000);
+		$(this).animate({height:'50px'}, 2000);
+		$(this).animate({width:'50px'}, 2000);
+		
+	});
+
+	$('#box5').click(function() {
+		$(this).animate({marginLeft:'5000px'}, 1000);
+		$(this).animate({height:'50px'}, 1000);
+		$(this).animate({width:'50px'}, 1000);
+		
+	});
+
+	$('#box6').click(function() {
+		$(this).animate({marginLeft:'5000px'}, 500);
+		$(this).animate({height:'50px'}, 500);
+		$(this).animate({width:'50px'}, 500);
+		
+	});
+
+	$('#box7').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box8').click(function() {
+		$(this).animate({borderRadius:'500px'}, 500);
+		$(this).animate({height:'50px'}, 500);	
+
+	});
+
+	$('#box9').click(function() {
+		$(this).animate({borderRadius:'500px'}, 1500);
+		$(this).animate({height:'50px'}, 1500);
+		$(this).animate({width:'500px'}, 1500);
+		
+	});
+
+	$('#box10').click(function() {
+		$(this).animate({borderRadius:'500px'}, 1000);
+		$(this).animate({height:'200px'}, 1000);
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+		
+	});
+
+	$('#box11').click(function() {
+		$(this).animate({width:'5000px'}, 500);	
+		$(this).addClass("animated" );
+		$(this).addClass("shake" );
+		$(this).addClass("infinite" );
+
+	});
+
+	$('#box12').click(function() {
+		$(this).animate({borderRadius:'500px'}, 500);
+		$(this).animate({height:'50px'}, 1500);	
+		$(this).animate({width:'500px'}, 2000);
+		$(this).animate({marginLeft:'500px'}, 1500);
+		$(this).animate({height:'4000px'}, 800);
+		$(this).animate({height:'50px'}, 1500);
+		$(this).animate({width:'50px'}, 500);
+
+	});
+//SLOW SHRINK // 
+	$('#box13').click(function() {
+		$(this).animate({height:'1800px'}, 4000);	
+		$(this).animate({width:'1800px'}, 4000);
+		$(this).animate({height:'1750px'}, 4000);
+		$(this).animate({height:'1750px'}, 4000);
+
+
+	});
+
+	$('#box14').click(function() {
+		$(this).animate({height:'1500px'}, 4000);	
+		$(this).animate({width:'1500px'}, 4000);
+		$(this).animate({height:'1200px'}, 4000);
+		$(this).animate({height:'1200px'}, 4000);
+
+	});
+
+	$('#box15').click(function() {
+		$(this).animate({height:'1200px'}, 4000);	
+		$(this).animate({width:'1200px'}, 4000);
+		$(this).animate({height:'800px'}, 4000);
+		$(this).animate({height:'800px'}, 4000);	
+
+	});
+
+	$('#box16').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);	
+
+	});
+
+	$('#box17').click(function() {
+		$(this).animate({height:'800px'}, 4000);	
+		$(this).animate({width:'800px'}, 4000);
+		$(this).animate({height:'400px'}, 4000);
+		$(this).animate({height:'400px'}, 4000);	
+
+	});
+
+	$('#box18').click(function() {
+		$(this).animate({height:'600px'}, 4000);	
+		$(this).animate({width:'600px'}, 4000);
+		$(this).animate({height:'300px'}, 4000);
+		$(this).animate({height:'300px'}, 4000);	
+
+	});
+
+	$('#box19').click(function() {
+		$(this).animate({height:'400px'}, 4000);	
+		$(this).animate({width:'400px'}, 4000);
+		$(this).animate({height:'200px'}, 4000);
+		$(this).animate({height:'200px'}, 4000);	
+
+	});
+
+	$('#box20').click(function() {
+		$(this).animate({height:'200px'}, 4000);	
+		$(this).animate({width:'200px'}, 4000);
+		$(this).animate({height:'100px'}, 4000);
+		$(this).animate({height:'100px'}, 4000);	
+
+	});
+
+	$('#box21').click(function() {
+		$(this).animate({marginLeft:'2000px'}, 4000);
+		$(this).animate({marginTop:'500px'}, 4000);	
+		$(this).animate({marginLeft:'1000px'}, 4000);
+		$(this).animate({marginBottom:'1000px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("flash" );
+		$(this).addClass("infinite" );
+		
+
+	});
+
+	$('#box22').click(function() {
+		$(this).animate({marginLeft:'200px'}, 4000);
+		$(this).animate({marginTop:'50px'}, 4000);	
+		$(this).animate({marginLeft:'100px'}, 4000);
+		$(this).animate({marginBottom:'100px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("flash" );
+		$(this).addClass("infinite" );
+		
+
+	});
+
+	$('#box23').click(function() {
+		$(this).animate({marginLeft:'5000px'}, 2000);
+		$(this).animate({marginTop:'5000px'}, 2000);	
+		$(this).animate({marginLeft:'100px'}, 2000);
+		$(this).addClass("animated" );
+		$(this).addClass("flash" );
+		$(this).addClass("infinite" );
+		
+
+	});
+
+	$('#box24').click(function() {
+		$(this).animate({marginLeft:'200px'}, 1000);
+		$(this).animate({marginTop:'500px'}, 1000);	
+		$(this).animate({marginLeft:'100px'}, 1000);
+		$(this).animate({marginBottom:'100px'}, 1000);
+		$(this).addClass("animated" );
+		$(this).addClass("flash" );
+		$(this).addClass("infinite" );
+
+	});
+
+	$('#box25').click(function() {
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({marginTop:'500px'}, 500);	
+		$(this).animate({marginLeft:'1000px'}, 500);
+		$(this).animate({marginBottom:'1000px'}, 500);
+		$(this).addClass("animated" );
+		$(this).addClass("flash" );
+		$(this).addClass("infinite" );
+		
+
+	});
+
+	$('#box26').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("rotateOut" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box27').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("rotateOutUpLeft" );
+		$(this).addClass("infinite" );	
+
+	});
+
+
+	$('#box28').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'7000px'}, 4000);
+		$(this).animate({height:'6000px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("rotateOutUpRight" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box29').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("rotateOutUpLeft" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box30').click(function() {
+		$(this).animate({height:'1000px'}, 1000);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 4000);
+		$(this).animate({height:'9000px'}, 1000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("bounce" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box31').click(function() {
+		$(this).animate({height:'1000px'}, 4000);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 600);
+		$(this).animate({height:'9000px'}, 600);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("bounce" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box32').click(function() {
+		$(this).animate({height:'1000px'}, 1000);	
+		$(this).animate({marginLeft:'2000px'}, 1000);
+		$(this).animate({width:'1000px'}, 1000);
+		$(this).animate({height:'9000px'}, 3000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("bounce" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box33').click(function() {
+		$(this).animate({height:'1000px'}, 500);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 1000);
+		$(this).animate({height:'9000px'}, 1000);
+		$(this).animate({height:'600px'}, 500);
+		$(this).addClass("animated" );
+		$(this).addClass("bounce" );
+		$(this).addClass("infinite" );	
+
+	});
+
+	$('#box34').click(function() {
+		$(this).animate({height:'1000px'}, 500);	
+		$(this).animate({marginLeft:'2000px'}, 500);
+		$(this).animate({width:'1000px'}, 5000);
+		$(this).animate({height:'9000px'}, 3000);
+		$(this).animate({height:'600px'}, 4000);
+		$(this).addClass("animated" );
+		$(this).addClass("bounce" );
+		$(this).addClass("infinite" );	
+
+	});
+
+
+	$('#box35').click(function() {
+		$(this).draggable();
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+
+	$('#box36').click(function() {
+		$(this).animate({width:'16px'}, 2000);
+		$(this).animate({height:'16px'}, 2000);
+		$(this).animate({borderRadius:'8px'});
+		$(this).animate({backgroundColor:'#e38bd9'});
+		$(this).animate({paddingRight:'20px'});
+		$(this).animate({paddingTop:'45px'});
+		$(this).addClass("animated" );
+		$(this).addClass("pulse" );
+
+		});
+
+
+	$('#box37').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+
+		});
+		
+
+	$('#box38').click(function() {
+		$(this).animate({width:'30px'});
+		$(this).animate({height:'36px'});
+		$(this).animate({borderRadius:'15px'} ,'fast');
+		$(this).addClass("animated" );
+		$(this).addClass("rotateOutUpRight" );
+
+		});
+
+	$('#box39').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box40').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box41').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box42').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box43').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box44').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box45').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box46').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box47').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box48').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box49').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+		});
+
+	$('#box50').click(function() {
+		$(this).addClass("animated" );
+		$(this).addClass("jello" );
+		$(this).addClass("infinite" );
+
+	});
+
+
+});
